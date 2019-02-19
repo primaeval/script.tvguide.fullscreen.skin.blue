50dc7b0cfba4c374b5d11eac86662268ddb5b62f
primaeval <primaeval@users.noreply.github.com>
2019-02-19 18:49:30 +0100

    0.17.1

---
+++ addon.py
@@ -0,0 +1,25 @@
+import xbmc,xbmcgui,xbmcaddon
+
+SKINS = [["Blue", "Blue"]]
+
+
+d = xbmcgui.Dialog()
+names = [s[0] for s in SKINS]
+skin = d.select("TV Guide Fullscreen - Set Default Skin", names)
+if skin > -1:
+    tvgf = xbmcaddon.Addon("script.tvguide.fullscreen")
+    if tvgf:
+        tvgf.setSetting('skin.source', '2')
+        tvgf.setSetting('skin.folder', 'special://home/addons/script.tvguide.fullscreen.skin.blue/')
+        tvgf.setSetting('skin.user', SKINS[skin][1])
+        
+        tvgf.setSetting('epg.video.pip', 'true')
+        tvgf.setSetting('program.image.scale', 'true')      
+
+        tvgf.setSetting('logos.enabled', 'false')
+        tvgf.setSetting('channel.logo', 'true')        
+        
+        tvgf.setSetting('program.background.image.source', '0')
+        tvgf.setSetting('program.background.texture.url', 'special://home/addons/script.tvguide.fullscreen.skin.blue/resources/skins/Blue/media/primary.jpg')
+
+        tvgf.setSetting('action.bar', 'false')
